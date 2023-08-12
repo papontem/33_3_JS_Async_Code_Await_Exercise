ca688aeab045490f68fe88d8abf86fcc09354cc2
papontemjetix <papontem@gmail.com>
2023-08-12 18:10:37 -0400

    added async func to get a fact about a number, can use web console to call function with a different num if desired. also wrapped said functions awaiting api call in a try catch block to grab axios XMLHttp hiccups.

--- README.md
+++ README.md
@@ -11,10 +11,9 @@ We will complete the following tasks using asynchronous functions and await prom
 
 ## Development
 This App was made using a WSL Ubunto distro  running from a VS Code desktop environment.
-To help me develop the app live i use a couple extensions.
-- Ritwick Dey : [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+Here are some extensions i use:
+- [Ritwick Dey](https://ritwickdey.github.io/#!/) : [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
 - [Prettier](https://prettier.io/) : [Prettier - Code formatter](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode)
-
 
 
 [Previous Repo]: https://github.com/papontem/33_2_JS_Async_Code_Promises_Exercise
--- js/nums/nums.js
+++ js/nums/nums.js
@@ -1,111 +1,124 @@
-$(document).ready(function () {
-    const baseUrl = 'http://numbersapi.com/';
-    
-    console.log("HELLO NUMBERS!");
+const baseUrl = "http://numbersapi.com";
 
-    // PART 1.1
-    // pick your favorite number or any number really
-    let favNum = 0;
-    console.log(`Current favorite number: ${favNum}`);
+console.log("HELLO NUMBERS!");
 
-    // // Create the URL to get json resposnse from the API
-    // // by adding json query at the end with our selected number
-    // const randFactJsonURL = baseUrl + `${num}?json`;
-    // const randFact = axios.get(randFactJsonURL)
+// PART 1.1
+// Make a request to the Numbers API (http://numbersapi.com/) to get a fact about your favorite number.
+// (Make sure you get back JSON by including the json query key, specific to this API. Details.
 
-    // // PROMISE CHAINING
-    // randFact
-    //     .then( res => {
-    //         // console.log("RESOLVED!!\nresponde.data:",res.data)
-    //         return res.data
-    //     })
-    //     .then(data => {
-    //         // console.log(`data.text: ${data.text}`);
-    //         console.log(`Random Fact about # ${data.number}: ${data.text}`);
+let favNum = 0;
+console.log(`Current favorite #: ${favNum}`);
 
-    //     })
-    //     .catch(error => console.error("REJECTED!!\nERROR:",error));
+async function getFact(num) {
+	console.log(`Getting a fact about #${num}`);
 
-    // PART 1.2
-    // function callback for if you wish get a random number 0-100
-    const getRandomInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
-    // make a list of numbers or use the random list of numbers
-    let numListPromises = [];
-    
-    // button listener to get more fatcs
-    $('#getRandomFactsBtn').on('click', function () {
+	// let { data: fact } = await axios.get(`${baseUrl}/l${num}?json`);
+	// console.log("Fact!:", fact.text);
+	// $('#nums-fact-list').empty();
+	// $('#nums-fact-list').append(`<li>${fact.text}</li>`);
 
-        numListPromises = []
-
-        // for loop to push 3 num api get promises into the nums list
-        for (let i = 0; i < 3; i++) {
-            numListPromises.push(
-                // pick between a) sequence or b) a random list 
-                // axios.get(baseUrl + `${i}?json`) // a) sequence
-                axios.get(baseUrl + `${getRandomInt()}?json`)  // b) random list
-            );
-        }
+	try {
+		// grab data from response returned by axios, then rename it to fact
+		let { data: fact } = await axios.get(`${baseUrl}/l${num}?json`);
+		console.log("Fact!:", fact.text);
+		$("#nums-fact-list").empty();
+		$("#nums-fact-list").append(`<li>${fact.text}</li>`);
+	} catch (error) {
         
-        Promise.all(numListPromises)
-        .then(numApiResponses => {
-            // uncomment if you want the list to refresh
-            $('#nums-fact-list').empty();
+        // console.error("Name:",error.name);
+        // console.error("Code:",error.code);
+        // console.error("message:",error.message);
+        // console.error("Request:",error.request);
+        // console.error("Response:",error.response);
 
-            numApiResponses.forEach(res => {
-                const randFact = `Random fact about # ${res.data.number}: ${res.data.text}`;
-                $('#nums-fact-list').append(`<li>${randFact}</li>`);
-            });
-        })
-        .catch(error => {
-            console.error('REJECTED!!\nERROR:', error);
-        });
-    });
+		console.error("Full On Error!!:\n",error);
+		
+        return;
+	}
+}
+getFact(0);
 
-    // part 1.3
-    $('#fav-number-form').on('submit', function (event) {
-        event.preventDefault(); // Prevent default form submission behavior
+// PART 1.2
+// Figure out how to get data on multiple numbers in a single request.
+// Make that request and when you get the data back, put all of the number facts on the page.
 
-        // Get the entered favorite number
-        favNum = $('#favNum').val();
-        const $warningMessage = $('#warning-message');
-        // Check if favNum is empty or not
+// function callback for if you wish get a random number 0-100
+const getRandomInt = (min = 0, max = 100) =>
+	Math.floor(Math.random() * (max - min + 1)) + min;
+// make a list of numbers or use the random list of numbers
+let numListPromises = [];
 
-        if (favNum.trim() === '') {
-            $warningMessage.text('Please enter a number.');
-            return; 
-        } else {
-            // Clear the warning message if input is valid
-            $warningMessage.text(''); 
-        }
+// button listener to get more fatcs
+$("#getRandomFactsBtn").on("click", function () {
+	numListPromises = [];
 
-        console.log(`Your selected favorite number: ${favNum}`);
-        
-        numListPromises = []
+	// for loop to push 3 num api get promises into the nums list
+	for (let i = 0; i < 3; i++) {
+		numListPromises.push(
+			// pick between a) sequence or b) a random list
+			// axios.get(baseUrl + `/${i}?json`) // a) sequence
+			axios.get(baseUrl + `/${getRandomInt()}?json`) // b) random list
+		);
+	}
 
-        // for loop to push 4 fav num api get promises into the nums list
-        for (let i = 0; i < 4; i++) {
-            numListPromises.push(
-                axios.get(baseUrl + `${favNum}?json`) 
-            );
-        }
+	Promise.all(numListPromises)
+		.then((numApiResponses) => {
+			$("#nums-fact-list").empty();
 
-        Promise.all(numListPromises)
-        .then(numApiResponses => {
-            // uncomment if you want the list to refresh
-            $('#nums-fact-list').empty();
-            $('#nums-fact-list').append(`<p> -- Heres some trivia about # ${favNum} --</p>`);
-            numApiResponses.forEach(res => {
-                const randFact = `${res.data.text}`;
-                $('#nums-fact-list').append(`<li>${randFact}</li>`);
-            });
-        })
-        .catch(error => {
-            console.error('REJECTED!!\nERROR:', error);
-        });
-
-    });
-
-
+			numApiResponses.forEach((res) => {
+				const randFact = `Rand Fact: ${res.data.text}`;
+				$("#nums-fact-list").append(`<li>${randFact}</li>`);
+			});
+		})
+		.catch((error) => {
+			console.error("REJECTED!!\nERROR:", error);
+		});
 });
 
+// part 1.3
+// Use the API to get 4 facts on your favorite number.
+// Once you have them all, put them on the page.
+// It’s okay if some of the facts are repeats.
+// pick your favorite number or any number really
 
+$("#fav-number-form").on("submit", function (event) {
+	event.preventDefault(); // Prevent default form submission behavior
+
+	// Get the entered favorite number
+	favNum = $("#favNum").val();
+	const $warningMessage = $("#warning-message");
+	// Check if favNum is empty or not
+
+	if (favNum.trim() === "") {
+		$warningMessage.text("Please enter a number.");
+		return;
+	} else {
+		// Clear the warning message if input is valid
+		$warningMessage.text("");
+	}
+
+	console.log(`Your selected favorite number: ${favNum}`);
+
+	numListPromises = [];
+
+	// for loop to push 4 fav num api get promises into the nums list
+	for (let i = 0; i < 4; i++) {
+		numListPromises.push(axios.get(baseUrl + `/${favNum}?json`));
+	}
+
+	Promise.all(numListPromises)
+		.then((numApiResponses) => {
+			// uncomment if you want the list to refresh
+			$("#nums-fact-list").empty();
+			$("#nums-fact-list").append(
+				`<p> -- Heres some trivia about # ${favNum} --</p>`
+			);
+			numApiResponses.forEach((res) => {
+				const randFact = `${res.data.text}`;
+				$("#nums-fact-list").append(`<li>${randFact}</li>`);
+			});
+		})
+		.catch((error) => {
+			console.error("REJECTED!!\nERROR:", error);
+		});
+});
